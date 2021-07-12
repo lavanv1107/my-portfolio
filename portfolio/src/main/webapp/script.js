6d3b99ddaf69664c5781b09fce6009f6e419f591
lavanv1107 <vatanaklavan@gmail.com>
2021-07-12 14:22:01 +0000

    Modify script with functions for new content

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,36 +1,26 @@
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ["There is no moment in life that can't be improved with pizza."];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+/* Preloader */
+var preloader = document.querySelector(".preloader")
+window.addEventListener("load", vanish);
+function vanish() {
+  preloader.classList.add("disappear");
 }
 
-async function showMessage() {
-  const responseFromServer = await fetch('/hello');
-  const textFromResponse = await responseFromServer.text();
+/* Side Navigation Menu*/
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("side-nav-menu").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
 
-  const messageBox = document.getElementById('message-box');
-  messageBox.innerText = textFromResponse;
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("side-nav-menu").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+/* Dark Mode Toggle */
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  
 }
